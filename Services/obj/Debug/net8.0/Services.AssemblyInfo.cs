e64a763b045a1a7a014149a55b302011c41ed9e8
valiusa <valentin.iv.vladimirov@gmail.com>
2024-09-04 20:05:07 +0300

    resolved problems with projec dependacies

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8ffa805a6815386ca753974c7d9810dcd6e6fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092cc1ce3526ad03198a0ae4705ef954aa9633cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
